640809f762f1cfa8d77b0b3448e20b74fcb8df5a
Roger Melich <rogermelich@gmail.com>
2017-02-09 16:59:47 +0100

    add request user

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,18 @@
 <?php
 
 //Route::group(['prefix' => 'v1','middleware' => 'auth:api'], function () {
-Route::group(['prefix' => 'v1','middleware' => 'cors'], function () {
+Route::group(['prefix' => 'v1', 'middleware' => ['cors', 'auth:api']], function () {
     Route::resource('task', 'TasksController');
     Route::resource('user', 'UsersController');
     Route::resource('user.task', 'UserTasksController');
-
+    //Add Route Get for /user
+    Route::get('/user', function (Request $request) {
+        return $request::user();
+    });
 });
+
+//Example Laravel API user request
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
