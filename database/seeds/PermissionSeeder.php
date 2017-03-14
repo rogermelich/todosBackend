784cdbf51e2c37f492f447af8b93c5f4542a8238
Roger Melich <rogermelich@gmail.com>
2017-03-14 18:56:30 +0100

    Add user permission

--- database/seeds/PermissionSeeder.php
+++ database/seeds/PermissionSeeder.php
@@ -11,6 +11,12 @@ class PermissionSeeder extends Seeder
         Permission::create(['name' => 'create-task']);
         Permission::create(['name' => 'update-task']);
         Permission::create(['name' => 'delete-task']);
+        Permission::create(['name' => 'show-user']);
+        Permission::create(['name' => 'view-user']);
+        Permission::create(['name' => 'create-user']);
+        Permission::create(['name' => 'update-user']);
+        Permission::create(['name' => 'delete-user']);
+    }
 
         //Role::create(['name' => 'admin']);
     }
