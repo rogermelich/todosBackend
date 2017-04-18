f3c4e976c3e1f1083772523f28dfcc14bec7ff25
Roger Melich <rogermelich@gmail.com>
2017-04-18 11:38:20 +0200

    Sync

--- app/User.php
+++ app/User.php
@@ -2,6 +2,7 @@
 
 namespace RogerMelich\TodosBackend;
 
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
@@ -9,12 +10,12 @@ use Spatie\Permission\Traits\HasRoles;
 
 /**
  * Class User.
- * 
+ *
  * @package App
  */
 class User extends Authenticatable
 {
-    use HasApiTokens,Notifiable, HasRoles;
+    use HasApiTokens, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -22,7 +23,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','api_token'
+        'name', 'email', 'password', 'api_token'
     ];
 
     /**
@@ -31,7 +32,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token','api_token'
+        'password', 'remember_token', 'api_token'
     ];
 
     public static function findOrFail($id)
@@ -41,5 +42,44 @@ class User extends Authenticatable
     public function tasks()
     {
         return $this->hasMany(Task::class);
+    }
+
+    /**
+     * A user can have many messages.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    /**
+     * A user can have many GCM tokens.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function gcmTokens()
+    {
+        return $this->hasMany(GcmToken::class);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function routeNotificationForGcm()
+    {
+        return $this->gcmTokens->pluck('registration_id')->toArray();
+    }
+
+    /**
+     * Send the password reset notification.
+     *
+     * @param  string $token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 }
