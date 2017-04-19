2b08e21163c398c9198dcd79eb8e6e5be07ac8b2
Roger Melich <rogermelich@gmail.com>
2017-04-19 05:08:44 +0200

    Reset Password

--- app/Mail/MailResetPassword.php
+++ app/Mail/MailResetPassword.php
@@ -7,7 +7,7 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class MailPasswordReset extends Mailable
+class MailResetPassword extends Mailable
 {
     use Queueable, SerializesModels;
 
--- app/Mail/MailResetQueue.php
+++ app/Mail/MailResetQueue.php
@@ -1,33 +1,11 @@
 <?php
 
-namespace RogerMelich\TodosBackend\Mail;
+namespace RpgerMelich\TodosBackend\Mail;
 
-use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
-use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class MailResetQueue extends Mailable
+class MailResetQueue extends Mailable implements ShouldQueue
 {
-    use Queueable, SerializesModels;
-
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
-
-    /**
-     * Build the message.
-     *
-     * @return $this
-     */
-    public function build()
-    {
-        return $this->view('view.name');
-    }
+    //
 }
