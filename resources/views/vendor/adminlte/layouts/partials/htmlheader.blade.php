8134e3f7162db2bb9bec057ab537bf483b499b88
Roger Melich <rogermelich@gmail.com>
2017-04-27 11:27:45 +0200

    Sync

--- resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
+++ resources/views/vendor/adminlte/layouts/partials/htmlheader.blade.php
@@ -20,4 +20,18 @@
             'echoServerURL' => env('LARAVEL_ECHO_SERVER_URL','http://localhost:6001'),
         ]) !!};
     </script>
+    <script>
+        //See https://laracasts.com/discuss/channels/vue/use-trans-in-vuejs
+        window.trans = @php
+            // copy all translations from /resources/lang/CURRENT_LOCALE/* to global JS variable
+            $lang_files = File::files(resource_path() . '/lang/' . App::getLocale());
+            $trans = [];
+            foreach ($lang_files as $f) {
+                $filename = pathinfo($f)['filename'];
+                $trans[$filename] = trans($filename);
+            }
+            $trans['adminlte_lang_message'] = trans('adminlte_lang::message');
+            echo json_encode($trans);
+        @endphp
+    </script>
 </head>
